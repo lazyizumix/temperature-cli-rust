82fc1336185b705304c60c1fafb361591939cfb0
lazyizumix <izumix.hkr@gmail.com>
2025-08-24 15:09:37 +0900

    feat: ユーザーが入力した値を元に摂氏と華氏に変換して出力する機能の実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("変換したい値を数値で入力してね！");
+    let value = get_line();
+
+    println!("あなたが入力した値: {}", value);
+
+    let fahrenheit = get_fahrenheit(value);
+    let celsius = get_celsius(value);
+
+    println!("摂氏 -> 華氏: {:.1}F°", fahrenheit);
+    println!("華氏 -> 摂氏: {:.1}C°", celsius);
+}
+
+/*
+ユーザーが入力した値をStringからF64に変換する
+ */
+fn get_line() -> f64 {
+    loop {
+        let mut line = String::new();
+
+        io::stdin().read_line(&mut line).expect("入力エラー");
+
+        match line.trim().parse() {
+            Ok(num) => break num,
+            Err(_) => {
+                println!("数値を入力してください！");
+                continue;
+            }
+        }
+    }
+}
+
+fn get_fahrenheit(value: f64) -> f64 {
+    value * 1.8 + 32.0
+}
+
+fn get_celsius(value: f64) -> f64 {
+    (value - 32.0) / 1.8
 }
